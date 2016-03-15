eefeeaa0c6c493505fc972a6ec27ffc393eb3b28
Derek Slager <derekslager@gmail.com>
2016-03-14 22:49:07 -0700

    Switch to Clojars version of lazytest.

--- external/lein-repo/project.clj
+++ external/lein-repo/project.clj
@@ -1,6 +1,5 @@
 (defproject lein-repo "0.2.1"
   :description "Grabbag extra lein functionality"
-  :dependencies [[com.stuartsierra/lazytest "1.1.2"]
+  :dependencies [[lazytest "1.2.3"]
                  [org.clojure/tools.namespace "0.2.4"]]
-  :eval-in-leiningen true
-  :repositories {"stuart-sierra" "http://stuartsierra.com/maven2"})
+  :eval-in-leiningen true)
