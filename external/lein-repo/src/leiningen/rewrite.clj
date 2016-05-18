5f2f7e8e3f68af7377da835b0c5bfabe6878fa27
Mike Fikes <mike@amperity.com>
2016-05-18 10:23:41 -0400

    Set up internal deps

--- external/lein-repo/src/leiningen/rewrite.clj
+++ external/lein-repo/src/leiningen/rewrite.clj
@@ -1,6 +1,7 @@
 (ns leiningen.rewrite
   (:import [java.io.PushbackReader]
-           (java.io PushbackReader))
+           (java.io PushbackReader)
+           (java.nio.file Files))
   (:require
     [clojure.pprint :as pprint]
     [clojure.java.io :as jio]
@@ -43,14 +44,33 @@
     (assert spec (str "Missing external dep " dep))
     spec))
 
+(defn setup-internal-dep
+  [dep current-project-path]
+  (let [spec (get-in plugin/repo-config ['internal-dependencies dep])]
+    (assert spec (str "Missing internal dep " dep))
+    (let [dep-project-path     (str plugin/repo-root "/" spec)
+          checkouts-dir        (jio/file current-project-path "checkouts")
+          _                    (.mkdir checkouts-dir)
+          _                    (Files/createSymbolicLink 
+                                 (.toPath (jio/file checkouts-dir (str dep))) 
+                                 (.toPath (jio/file dep-project-path)) 
+                                 (make-array java.nio.file.attribute.FileAttribute 0))
+          _                    (println "Creating symlink" dep "->" dep-project-path)
+          dep-project-clj-path (str dep-project-path "/project.clj")
+          dep-project-clj      (read-project-edn (jio/as-file dep-project-clj-path))
+          dep-version          (nth dep-project-clj 2)]
+      [dep dep-version])))
+
 (defn updated-project-edn
   [^java.io.File project-clj]
-  (let [project-edn (read-project-edn project-clj)
-        required    ('required-dependencies plugin/repo-config)
-        external    (get-from-proj project-edn :external-dependencies)
-        qualified   (map qualify-dep external)
-        direct      (get-from-proj project-edn :dependencies)
-        combined    (vec (concat required qualified direct))]
+  (let [project-edn   (read-project-edn project-clj)
+        required      ('required-dependencies plugin/repo-config)
+        external      (get-from-proj project-edn :external-dependencies)
+        ext-qualified (map qualify-dep external)
+        internal      (get-from-proj project-edn :internal-dependencies)
+        int-qualified (map #(setup-internal-dep % (.getParent project-clj)) internal)
+        direct        (get-from-proj project-edn :dependencies)
+        combined      (vec (concat required direct ext-qualified int-qualified))]
     (set-in-proj project-edn :dependencies combined)))
 
 (defn backup-file
@@ -66,7 +86,7 @@
   (jio/delete-file target (not destroy?))
   (.renameTo source target))
 
-(defn rewrite-project-file
+(defn rewrite-project
   [^java.io.File project-clj]
   (let [canonical-path (.getCanonicalPath project-clj)]
     (println "Inspecting" canonical-path)
@@ -77,19 +97,25 @@
                           (binding [pprint/*print-right-margin* 70] 
                             (pprint/pprint updated)))))))
 
-(defn restore-project-file
+(defn restore-project
   [^java.io.File project-clj]
   (let [backup (backup-file project-clj)]
     (when (.exists backup)
       (println "Restoring" (.getCanonicalPath project-clj))
-      (move-file backup project-clj true))))
+      (move-file backup project-clj true))
+    (let [checkouts-dir (jio/file (.getParent project-clj) "checkouts")]
+      (when (.exists checkouts-dir)
+        (println "Removing" (.getCanonicalPath checkouts-dir))
+        (doseq [symlink (.listFiles checkouts-dir)]
+          (jio/delete-file symlink))
+        (jio/delete-file checkouts-dir)))))
 
 (defn rewrite
   "Rewrite lein-repo project files using explicit references."
   [project & args]
   (if-let [repo-root plugin/repo-root]
     (run! (if (not= ["undo"] args)
-            rewrite-project-file
-            restore-project-file)
+            rewrite-project
+            restore-project)
       (find-project-files repo-root))
     (println "Not in a lein-repo monorepo.")))
